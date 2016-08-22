100843fd7b4a41764d4b5680aee9adc46bcb8dff
thedmd <michcic@gmail.com>
2016-08-22 17:46:02 +0200

    Application: Remove separator

--- Editor/Application/application.cpp
+++ Editor/Application/application.cpp
@@ -319,7 +319,7 @@ void Application_Frame()
     auto& io = ImGui::GetIO();
 
     ImGui::Text("FPS: %.2f (%.2gms)", io.Framerate, io.Framerate ? 1000.0f / io.Framerate : 0.0f);
-    ImGui::Separator();
+    //ImGui::Separator();
     //ImGui::Spacing();
 
     auto iconSize2 = size(24, 24);
