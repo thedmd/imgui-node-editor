108d220aded603516be83699ef501ebc6ea1dd38
Rui Mori <rui.mori@dpc-net.co.jp>
2019-11-13 15:45:45 +0900

    crude_json: Fix to accept number exactly zero

--- NodeEditor/Source/crude_json.cpp
+++ NodeEditor/Source/crude_json.cpp
@@ -575,7 +575,7 @@ private:
             if (end != n.c_str() + n.size())
                 return false;
 
-            if (!std::isnormal(v))
+            if (v != 0 && !std::isnormal(v))
                 return false;
 
             result = v;
