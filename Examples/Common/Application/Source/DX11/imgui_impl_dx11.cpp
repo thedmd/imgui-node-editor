2522c65355879b68ad8c39f05634f92cc8d2bf98
thedmd <michcic@gmail.com>
2020-04-17 19:43:59 +0200

    Enable support for 64k+ meshes with 16-bit indices

--- Examples/Common/Application/Source/DX11/imgui_impl_dx11.cpp
+++ Examples/Common/Application/Source/DX11/imgui_impl_dx11.cpp
@@ -500,7 +500,7 @@ bool    ImGui_ImplDX11_Init(ID3D11Device* device, ID3D11DeviceContext* device_co
     // Setup back-end capabilities flags
     ImGuiIO& io = ImGui::GetIO();
     io.BackendRendererName = "imgui_impl_dx11";
-    //io.BackendFlags |= ImGuiBackendFlags_RendererHasVtxOffset;  // We can honor the ImDrawCmd::VtxOffset field, allowing for large meshes.
+    io.BackendFlags |= ImGuiBackendFlags_RendererHasVtxOffset;  // We can honor the ImDrawCmd::VtxOffset field, allowing for large meshes.
 
     // Get factory from device
     IDXGIDevice* pDXGIDevice = NULL;
