e776196d418614473b3aa6069989bfb170ab8111
Thomas Topp <thomas@topp.cc>
2021-03-01 16:58:56 +0100

    Default initialize Flow() reverseDirection parameter to false

--- imgui_node_editor.h
+++ imgui_node_editor.h
@@ -270,7 +270,7 @@ ImDrawList* GetNodeBackgroundDrawList(NodeId nodeId);
 
 bool Link(LinkId id, PinId startPinId, PinId endPinId, const ImVec4& color = ImVec4(1, 1, 1, 1), float thickness = 1.0f);
 
-void Flow(LinkId linkId, bool reverseDirection);
+void Flow(LinkId linkId, bool reverseDirection = false);
 
 bool BeginCreate(const ImVec4& color = ImVec4(1, 1, 1, 1), float thickness = 1.0f);
 bool QueryNewLink(PinId* startId, PinId* endId);
