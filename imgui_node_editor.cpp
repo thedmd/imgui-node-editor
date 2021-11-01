0e30282cbdbae28ac333039118d13e766c0a5593
lance <1069421816@qq.com>
2021-11-01 15:09:04 +0800

    Update imgui_node_editor.cpp
    [fix] IM_ASSERT(size_arg.x != 0.0f && size_arg.y != 0.0f);

--- imgui_node_editor.cpp
+++ imgui_node_editor.cpp
@@ -2015,6 +2015,8 @@ ed::Control ed::EditorContext::BuildControl(bool allowOffscreen)
         // debug
         //if (id < 0) return ImGui::Button(idString, to_imvec(rect.size));
 
+        if(rect.GetWidth() == 0 && rect.GetHeight() == 0)
+            return false;
         auto result = ImGui::InvisibleButton(idString, rect.GetSize());
 
         // #debug
