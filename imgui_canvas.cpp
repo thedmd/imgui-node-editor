d79ebdeb2375fa6c1f88a40e52e7d08c8d472b62
thedmd <michcic@gmail.com>
2021-11-18 10:16:11 +0100

    Canvas: Update call ImGui::IsClippedEx() on ImGui > 18415 (#138)

--- imgui_canvas.cpp
+++ imgui_canvas.cpp
@@ -101,8 +101,13 @@ bool ImGuiEx::Canvas::Begin(ImGuiID id, const ImVec2& size)
 
     UpdateViewTransformPosition();
 
+# if IMGUI_VERSION_NUM > 18415
+    if (ImGui::IsClippedEx(m_WidgetRect, id))
+        return false;
+# else
     if (ImGui::IsClippedEx(m_WidgetRect, id, false))
         return false;
+# endif
 
     // Save current channel, so we can assert when user
     // call canvas API with different one.
