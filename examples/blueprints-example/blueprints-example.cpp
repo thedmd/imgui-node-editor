be0e8128291e3e3a4c2f4d99e39085b3308acce0
thedmd <michcic@gmail.com>
2023-08-31 22:52:34 +0200

    Examples: Don't use deprecated SetItemAllowOverlap (#250)

--- examples/blueprints-example/blueprints-example.cpp
+++ examples/blueprints-example/blueprints-example.cpp
@@ -766,6 +766,9 @@ struct Example:
             }
 
             bool isSelected = std::find(selectedNodes.begin(), selectedNodes.end(), node.ID) != selectedNodes.end();
+# if IMGUI_VERSION_NUM >= 18967
+            ImGui::SetNextItemAllowOverlap();
+# endif
             if (ImGui::Selectable((node.Name + "##" + std::to_string(reinterpret_cast<uintptr_t>(node.ID.AsPointer()))).c_str(), &isSelected))
             {
                 if (io.KeyCtrl)
@@ -794,7 +797,11 @@ struct Example:
 
             auto drawList = ImGui::GetWindowDrawList();
             ImGui::SetCursorScreenPos(iconPanelPos);
+# if IMGUI_VERSION_NUM < 18967
             ImGui::SetItemAllowOverlap();
+# else
+            ImGui::SetNextItemAllowOverlap();
+# endif
             if (node.SavedState.empty())
             {
                 if (ImGui::InvisibleButton("save", ImVec2((float)saveIconWidth, (float)saveIconHeight)))
@@ -814,7 +821,11 @@ struct Example:
             }
 
             ImGui::SameLine(0, ImGui::GetStyle().ItemInnerSpacing.x);
+# if IMGUI_VERSION_NUM < 18967
             ImGui::SetItemAllowOverlap();
+# else
+            ImGui::SetNextItemAllowOverlap();
+# endif
             if (!node.SavedState.empty())
             {
                 if (ImGui::InvisibleButton("restore", ImVec2((float)restoreIconWidth, (float)restoreIconHeight)))
@@ -838,7 +849,9 @@ struct Example:
             }
 
             ImGui::SameLine(0, 0);
+# if IMGUI_VERSION_NUM < 18967
             ImGui::SetItemAllowOverlap();
+# endif
             ImGui::Dummy(ImVec2(0, (float)restoreIconHeight));
 
             ImGui::PopID();
